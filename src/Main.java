f7f89c3fc512b90bde8ef81495b5b7e68dc878d2
vpm789 <vpm789@gmail.com>
2023-02-20 15:58:14 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+//        task2();
+//        task3();
+    }
+    private static void task1() {
+        System.out.println("Задача №1");
+    }
+    private static void task2() {
+        System.out.println("");
+        System.out.println("Задача №2");
+    }
+    private static void task3() {
+        System.out.println("");
+        System.out.println("Задача №3");
+    }
+}
